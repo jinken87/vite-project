cb1fb6a210ed9737b70df38e8e31942bf42f219a
jinken87 <ken87208@gmail.com>
2023-07-04 17:08:52 +0800

    upload

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from 'unplugin-vue-components/resolvers';
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [
     vue(),
@@ -11,7 +11,7 @@ export default defineConfig({
     }),
   ],
   base:"/",
-  publicPath:process.env.NODE_ENV === 'production'
-  ?/vite-project/
-  :'/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? 'vite-project'
+    : '/'
 })
