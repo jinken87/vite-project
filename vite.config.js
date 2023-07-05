acd846e6af4e63ce72dee529f43fd4d98a7c9ec4
jinken87 <ken87208@gmail.com>
2023-07-05 10:46:11 +0800

    2

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,33 @@ import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from 'unplugin-vue-components/resolvers';
 
 export default defineConfig({
-  base:"/",
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vite-project/'
-    : '/',
+  base:"/vite-project/",
   plugins: [
     vue(),
     Components({
       resolvers: [VantResolver()],
     }),
-  ]
+  ],
+  resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+      }
 })
+// import { fileURLToPath, URL } from 'node:url'
+
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: '/private/',
+//   plugins: [
+//     vue(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
