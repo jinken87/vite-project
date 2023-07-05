cf8810ec2f394d013a884fc04a86425b33397eb2
jinken87 <ken87208@gmail.com>
2023-07-05 10:31:28 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,14 @@ import Components from 'unplugin-vue-components/vite';
 import { VantResolver } from 'unplugin-vue-components/resolvers';
 
 export default defineConfig({
+  base:"/",
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/vite-project/'
+    : '/',
   plugins: [
     vue(),
     Components({
       resolvers: [VantResolver()],
     }),
-  ],
-  base:"/",
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/vite-project/'
-    : '/'
+  ]
 })
